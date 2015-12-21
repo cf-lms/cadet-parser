e37e2a4580e0810620195e7a829b763b5dfa4db7
timcmiller <timcmiller@live.com>
2015-12-20 21:08:56 -0800

    removed extra lines

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 var parser = module.exports = require(__dirname + '/lib/parser.js');
 
-var text = '#JavaScript: An eight week course over the MEAN stack.\n##Week 1\n###Day 1\n####Code: Simple Test and Modular Parrerns\n#####Rubric: 10\n####Read: Basic Testing With Mocha/Chai\n#####Rubric: 5\n\n###Day 2\n####Code: Adding a Gulpfile/package.json\n#####Rubric: 10\n####Read: Semantic Versioning\n#####Rubric: 5\n';
-
-parser(text, function(course) {
-
-  debugger;
-});
 
 
